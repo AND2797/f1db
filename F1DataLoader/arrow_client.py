e461860ea31c3e1d1b8d0ef55a4a6f25a5289678
AND2797 <aditya.das2797@gmail.com>
2025-03-26 21:23:45 +0000

    arrow client in data loader

---
+++ F1DataLoader/arrow_client.py
@@ -0,0 +1,21 @@
+import pyarrow.flight as flight
+
+
+class ArrowDuckDBClient:
+
+    def __init__(self, conn):
+        self.conn = conn
+        self._client = flight.connect(conn)
+
+    def attach_new_table(self, json):
+        """
+        When a new .duckb table is created via dataloader, this call will get invoked to update the ArrowFlight source
+        :param json:
+        :return:
+        """
+        action = flight.Action("attach_new_table", json.encode())
+        results = self._client.do_action(action)
+        return results
+
+
+arrow_duckdb_client = ArrowDuckDBClient("grpc://localhost:8815")
